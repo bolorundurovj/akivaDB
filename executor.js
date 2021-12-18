e12f430057ab909dc75b8ade470c10f163727278
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-12-18 09:53:56 +0100

    Update-Executor

--- executor.js
+++ executor.js
@@ -1,19 +1,18 @@
-let results = [];
+const results = []
 const executeCommand = function (statements) {
-	if (statements.length === 1) {
-		return statements[0].command.execute(statements[0].params);
-	}
+  if (statements.length === 1) {
+    return statements[0].command.execute(statements[0].params)
+  } else {
+    for (let i = 0; i < statements.length; i++) {
+      const { command, params } = statements[i]
 
+      const result = command.execute(params)
 
-	for (let i = 0; i < statements.length; i++) {
-		const { command, params } = statements[i];
+      results.push(result)
+    }
 
-		let result = command.execute(params);
+    return results
+  }
+}
 
-		results.push(result);
-	}
-
-	return results;
-};
-
-module.exports = { executeCommand };
+module.exports = { executeCommand }
