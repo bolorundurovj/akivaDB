493c93671c3d09685557002579af30bd5a1b66eb
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-12-21 21:52:29 +0100

    Setup-Command-Executor

---
+++ commandExecutor.js
@@ -0,0 +1,21 @@
+const select = require('./commands/select')
+const insert = require('./commands/insert')
+const createCollection = require('./commands/createCollection')
+const using = require('./commands/using')
+const deleteCommand = require('./commands/delete')
+const update = require('./commands/update')
+const bulkInsert = require('./commands/bulkInsert')
+const createIndex = require('./commands/createIndex')
+
+const commands = [
+  select,
+  insert,
+  createCollection,
+  using,
+  deleteCommand,
+  update,
+  bulkInsert,
+  createIndex
+]
+
+module.exports = { commands }
