b98d6b6f6e156e8068cacea84ac027e7577cda3c
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-12-21 21:54:29 +0100

    Setup-Command-Evaluator

---
+++ evaluator.js
@@ -0,0 +1,32 @@
+const parser = require('./parser')
+const { executeCommand } = require('./executor')
+
+function evalString (cmd, context, filename, callback) {
+  cmd = cmd.replace('\n', '')
+
+  if (cmd === '') {
+    return callback(null, undefined)
+  } else {
+    const eachStatement = parser.parseStatements(cmd)
+
+    console.time('command time')
+
+    const commandResult = executeCommand(eachStatement)
+
+    console.timeEnd('command time')
+
+    console.log(commandResult)
+
+    callback(null, 'Done.')
+  }
+}
+
+function evalObject (cmdObject) {
+  const eachStatement = parser.objStatementToCommand(cmdObject)
+
+  const commandResult = executeCommand(eachStatement)
+
+  return commandResult
+}
+
+module.exports = { evalString, evalObject }
