3f101c5da6835a436bc8be3c25aeba3bbab3ef4e
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-12-21 21:53:04 +0100

    Setup-Parser

---
+++ parser.js
@@ -0,0 +1,30 @@
+const { commands } = require('./commandExecutor')
+const parser = require('akivaparser')
+
+function parseStatements (str) {
+  const statements = parser.parseInput(str)
+
+  return objStatementToCommand(statements)
+}
+
+function objStatementToCommand (statements) {
+  if (!Array.isArray(statements)) {
+    statements = [statements]
+  }
+
+  statements = statements.map((st) => {
+    return {
+      command: commands.find(
+        (cm) => cm.name.toLowerCase() === st.type.toLowerCase()
+      ),
+      params: st.params
+    }
+  })
+
+  return statements
+}
+
+module.exports = {
+  parseStatements,
+  objStatementToCommand
+}
