6c736491accb9542c7a8955b7835f29bf3fc22c3
Valiant-Joshua Bolorunduro <valiant-joshuabolorunduro@Valiant-Joshuas-MacBook-Pro.local>
2022-01-16 20:10:06 +0100

    Update

--- transfer.js
+++
@@ -1,5 +0,0 @@
-const now = Date.now()
-
-require('./lib').deprecated('database', 'Variables').then(() => console.log(`${Date.now()-now}ms`))
-
-process.once('beforeExit', () => console.log(`${Date.now()-now}ms`))
