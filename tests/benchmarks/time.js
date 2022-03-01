10b3edbe493e9f92c88bdd154b8d6fbc76069345
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-03-01 10:49:01 +0100

    Update_Timing_Benchmarks

--- tests/benchmarks/time.js
+++ tests/benchmarks/time.js
@@ -1,21 +1,18 @@
-// Check the processing time for each of the DB operations with load
-const AkivaDB = require('../../lib');
-let db = AkivaDB("database");
+// Check the processing time for each of the DB operations
+const AkivaDB = require('../../lib/main').default;
+const saved = new AkivaDB({ name: 'saved', root: 'akivadb', inMemory: false });
+const inmemory = new AkivaDB({ name: 'inmemory', root: 'akivadb', inMemory: true });
 
-(async () => {
-    db = await db.getDB();
-    console.log(db.name, db.isReady, collection.name, collection.ready);
-})()
-
-const collection = db.collection({
-    name: "test"
-});
 
 // delete all records
 (async () => {
-    await collection.deleteMany({});
+    await saved.drop();
+    await inmemory.deleteMany();
 })()
 
+/**
+ * 1,000,000 default
+ */
 let x = 1e6;
 
 let arg = parseInt(process.argv[2]);
@@ -25,53 +22,55 @@ if (process.argv[2] && typeof (arg) === "number") {
 }
 
 
-console.log('/****************** Test for ' + x + ' documents(s) *************/');
+console.log('/****************** Test for ' + x + ' documents(s) persisted *************/');
+
+console.log("File size at beginning : ", saved.size, saved.fileSize);
 
 console.time(x + ' : Insert(s)');
 for (var i = 0; i < x; i++) {
     (async () => {
-        await collection.insert({
+        await saved.insert({
             title: 'AkivaDB rocks ' + i,
             published: 'today ' + i,
             rating: '5 stars ' + i
-        }, { _id: i })
+        })
     })()
 };
 console.timeEnd(x + ' : Insert(s)');
 
 console.time(x + ' : Find without query');
 (async () => {
-    let resp = await collection.find({});
+    let resp = await saved.find({});
     console.log(resp);
 })()
 console.timeEnd(x + ' : Find without query');
 
 console.time(x + ' : Find with query');
 (async () => {
-    let resp = await collection.find({ _id: '10' });
+    let resp = await saved.find({ title: 'AkivaDB rocks 1' });
     console.log(resp);
 })()
 console.timeEnd(x + ' : Find with query');
 
 console.time(x + ' : Find One without query');
 (async () => {
-    await collection.findOne({});
+    await saved.findOne({});
 })()
 console.timeEnd(x + ' : Find One without query');
 
 console.time(x + ' : Find One with query');
 (async () => {
-    await collection.findOne({ _id: '10' });
+    await saved.findOne({ title: 'AkivaDB rocks 1' });
 })()
 console.timeEnd(x + ' : Find One with query');
 
 console.time(x + ' : Update');
 (async () => {
-    await collection.update({ _id: '10' }, 'title', 'AkivaDB is awesome');
+    await saved.update({ title: 'AkivaDB rocks 1' }, 'title', 'AkivaDB is awesome');
 })()
 console.timeEnd(x + ' : Update');
 
-// console.log("File size : ", collection.size);
+console.log("File size at end : ", saved.size, saved.fileSize);
 
 
 console.log('/****************** Test for ' + x + ' documents(s) *************/\n\n');
