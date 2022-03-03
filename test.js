43963905e685830bc3f16461de437f71bc39da2e
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-03-03 12:28:43 +0100

    Update

--- test.js
+++ test.js
@@ -2,26 +2,28 @@ const AkivaDB = require('./lib/main').default;
 
 const users = new AkivaDB({ name: 'users', root: 'akivadb', inMemory: false });
 const classes = new AkivaDB({ name: 'classes', root: 'akivadb', inMemory: false });
+let id;
 
 console.log('users db', users.name, users.size, users.memoryMode, users.fileSize);
 console.log('classes db', classes.name, classes.size, classes.memoryMode, classes.fileSize, classes.version);
 
-users.on("insert", (x) => {
-    console.log(x._id, "users db");
-})
+// users.on("insert", (x) => {
+//     console.log(x._id, "users db");
+// })
 
-classes.on("insert", (x) => {
-    console.log(x._id, "classes db");
-})
-classes.on("delete", (x) => {
-    console.log(x, "classes db del");
-})
+// classes.on("insert", (x) => {
+//     console.log(x._id, "classes db");
+// })
+// classes.on("delete", (x) => {
+//     console.log(x, "classes db del");
+// })
 
 let x = 1;
 let y = 10;
 while (x > 0) {
     users.insert({ name: `Jane Doe - ${x}`, date: new Date(), obj: { a: x, b: Math.random() } }).then((a) => {
         // console.log(a);
+        id = a._id;
     })
     // users.insert(`Jane Doe - ${x}`).then((a) => {
     //     // console.log(a);
@@ -32,8 +34,8 @@ while (x > 0) {
 
 let arr = Array.from({ length: y }, (x, i) => ({ name: `Class ${y}`, students: i + 1, createdAt: new Date() }))
 classes.insertMany(arr).then((a) => {
-    console.log(a);
-})
+    // console.log(a);
+});
 
 // classes.findById(["17f2b1123fd9c1fae09ff63", "17f2b11272c43d62ce7b716"]).then(x => {
 //     console.log(x);
@@ -65,3 +67,9 @@ classes.insertMany(arr).then((a) => {
 // })
 
 // classes.drop()
+users.updateMany(
+    { name: "Jane Doe - 1" },
+    { name: "AkivaDB is awesome" }
+).then((x) => {
+    console.log(x);
+})
--- tests/benchmarks/time.js
+++ tests/benchmarks/time.js
@@ -2,9 +2,9 @@
 const AkivaDB = require("../../lib/main").default;
 const saved = new AkivaDB({ name: "saved", root: "akivadb", inMemory: false });
 const inmemory = new AkivaDB({
-  name: "inmemory",
-  root: "akivadb",
-  inMemory: true,
+    name: "inmemory",
+    root: "akivadb",
+    inMemory: true,
 });
 
 let ID = "inmemory";
@@ -12,8 +12,8 @@ let IDS = [];
 
 // delete all records
 (async () => {
-  await saved.drop();
-  await inmemory.deleteMany();
+    await saved.drop();
+    await inmemory.deleteMany();
 })();
 
 let dumpData = (value) => {
@@ -30,109 +30,108 @@ let x = 1e6;
 let arg = parseInt(process.argv[2]);
 
 if (process.argv[2] && typeof arg === "number") {
-  x = arg;
+    x = arg;
 }
 
 console.log(
-  "/****************** Test for " + x + " documents(s) persisted *************/"
+    "/****************** Test for " + x + " documents(s) persisted *************/"
 );
 
 console.log("File size at beginning : ", saved.size, saved.fileSize);
 
 console.time(x + " : Insert(s)");
 for (var i = 0; i < x; i++) {
-  // (async () => {
-  //     await saved.insert({
-  //         title: "AkivaDB rocks " + i,
-  //         published: "today " + i,
-  //         rating: "5 stars " + i,
-  //     });
-  // })();
-  saved
-    .insert({
-      title: "AkivaDB rocks " + i,
-      published: "today " + i,
-      rating: "5 stars " + i,
-    })
-    .then((x) => {
-      console.log(1);
-      ID = x._id;
-      IDS.push(x._id);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    // (async () => {
+    //     await saved.insert({
+    //         title: "AkivaDB rocks " + i,
+    //         published: "today " + i,
+    //         rating: "5 stars " + i,
+    //     });
+    // })();
+    saved
+        .insert({
+            title: "AkivaDB rocks " + i,
+            published: "today " + i,
+            rating: "5 stars " + i,
+        })
+        .then((x) => {
+            console.log(1);
+            ID = x._id;
+            IDS.push(x._id);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 }
 console.timeEnd(x + " : Insert(s)");
 
 console.time(x + " : Insert Many");
 let arr = Array.from({ length: x }, (a, i) => ({
-  name: `Class ${x}`,
-  students: i + 1,
-  createdAt: new Date(),
+    name: `Class ${x}`,
+    students: i + 1,
+    createdAt: new Date(),
 }));
 // saved.insertMany(arr).then((s) => {
 //     console.log(s);
 // })
 async () => {
-  await saved.insertMany(arr);
+    await saved.insertMany(arr);
 };
 console.timeEnd(x + " : Insert Many");
 
 console.time(x + " : Find without query");
 (async () => {
-  let resp = await saved.find({});
-  console.log(resp);
+    let resp = await saved.find({});
+    console.log(resp);
 })();
 console.timeEnd(x + " : Find without query");
 
 console.time(x + " : Find with query");
 (async () => {
-  let resp = await saved.find({ title: "AkivaDB rocks 1" });
-  console.log(resp);
+    let resp = await saved.find({ title: "AkivaDB rocks 1" });
+    console.log(resp);
 })();
 console.timeEnd(x + " : Find with query");
 
 console.time(x + " : Find One without query");
 (async () => {
-  await saved.findOne({});
+    await saved.findOne({});
 })();
 console.timeEnd(x + " : Find One without query");
 
 console.time(x + " : Find One with query");
 (async () => {
-  await saved.findOne({ title: "AkivaDB rocks 1" });
+    await saved.findOne({ title: "AkivaDB rocks 1" });
 })();
 console.timeEnd(x + " : Find One with query");
 
 console.time(x + " : Find One by ID");
 (async () => {
-  let a = await saved.findOneById(ID);
-  console.log(a);
+    let a = await saved.findOneById(ID);
+    console.log(a);
 })();
 console.timeEnd(x + " : Find One by ID");
 
 console.time(x + " : Find Many by ID");
 (async () => {
-  let a = await saved.findById([...IDS]);
-  console.log(a);
+    let a = await saved.findById([...IDS]);
+    console.log(a);
 })();
 console.timeEnd(x + " : Find Many by ID");
 
-console.time(x + " : Update");
+console.time(x + " : Update One");
 (async () => {
-  await saved.update(
-    { title: "AkivaDB rocks 1" },
-    "title",
-    "AkivaDB is awesome"
-  );
+    await saved.updateOne(
+        { title: "AkivaDB rocks 1" },
+        { title: "AkivaDB is awesome" }
+    );
 })();
-console.timeEnd(x + " : Update");
+console.timeEnd(x + " : Update One");
 
 console.log("File size at end : ", saved.size, saved.fileSize);
 
 console.log(
-  "/****************** Test for " + x + " documents(s) *************/\n\n"
+    "/****************** Test for " + x + " documents(s) *************/\n\n"
 );
 
 process.exit();
