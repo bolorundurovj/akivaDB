571187c3acf43209c29b70ccde8bc54c5bdd1efb
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-03-04 16:34:53 +0100

    Update_Test

--- test.js
+++ test.js
@@ -1,67 +1,98 @@
-const AkivaDB = require('./lib/main').default;
+const AkivaDB = require("./lib/main").default;
 
-const users = new AkivaDB({ name: 'users', root: 'akivadb', inMemory: false });
-const classes = new AkivaDB({ name: 'classes', root: 'akivadb', inMemory: false });
+const classes = new AkivaDB({
+  name: "classes",
+  root: "akivadb",
+  inMemory: false,
+});
+console.log(
+  "classes db",
+  classes.name,
+  classes.size,
+  classes.memoryMode,
+  classes.fileSize,
+  classes.version
+);
 
-console.log('users db', users.name, users.size, users.memoryMode, users.fileSize);
-console.log('classes db', classes.name, classes.size, classes.memoryMode, classes.fileSize, classes.version);
-
-users.on("insert", (x) => {
-    console.log(x._id, "users db");
-})
+let updateObj = { _id: "classes" };
 
 classes.on("insert", (x) => {
-    console.log(x._id, "classes db");
-})
+  updateObj = x;
+  console.log(x._id, "insert");
+});
 classes.on("delete", (x) => {
-    console.log(x, "classes db del");
-})
+  console.log(x._id, "delete");
+});
+classes.on("update", (x) => {
+  console.log(x._id, "update");
+});
 
-let x = 1;
-let y = 10;
+let y,
+  x = 10;
 while (x > 0) {
-    users.insert({ name: `Jane Doe - ${x}`, date: new Date(), obj: { a: x, b: Math.random() } }).then((a) => {
-        // console.log(a);
+  classes
+    .insert({
+      name: `Class ${x}`,
+      students: x + Math.round(Math.random() * 1e15),
+      createdAt: new Date(),
     })
-    // users.insert(`Jane Doe - ${x}`).then((a) => {
-    //     // console.log(a);
-    // })
-
-    x--;
+    .then((a) => {
+      // console.log(a);
+    });
+  x--;
 }
 
-let arr = Array.from({ length: y }, (x, i) => ({ name: `Class ${y}`, students: i + 1, createdAt: new Date() }))
-classes.insertMany(arr).then((a) => {
-    // console.log(a);
-})
+let arr = Array.from({ length: y }, (x, i) => ({
+  name: `Class ${y}`,
+  students: i + 1,
+  createdAt: new Date(),
+}));
 
-// classes.findById(["17f2b1123fd9c1fae09ff63", "17f2b11272c43d62ce7b716"]).then(x => {
-//     console.log(x);
-// })
+(async () => {
+  await classes.insertMany(arr);
+})();
 
+classes.findById([updateObj._id, "17f2b11272c43d62ce7b716"]).then((x) => {
+  console.log(x);
+});
 
-// users.findOne({ $string: { name: "Jane" } }, { projection: ['name', '_id'] }).then(c => {
-//     console.log(c);
-// })
+classes
+  .findOne({ $string: { name: "class" } }, { projection: ["name", "_id"] })
+  .then((c) => {
+    console.log(c);
+  });
 
 // classes.find().then(c => {
 //     console.log(c);
 // })
 
-// classes.deleteOneById("17f37d00d29117e5d0ce35").then((x) => {
-//     console.log(x);
+updateObj.name = `Jane Doe - new`;
+classes.updateById(updateObj._id, updateObj).then((a) => {
+  // console.log(a);
+});
+
+classes.deleteOneById("17f37d00d29117e5d0ce35").then((x) => {
+  console.log(x);
+});
+
+classes
+  .deleteById(["17f37d00d29117e5d0ce35", "17f37c694052c36340add59"])
+  .then((x) => {
+    console.log(x);
+  });
+
+classes
+  .deleteById(["17f37d00d29117e5d0ce35", "17f37c694052c36340add59"])
+  .then((x) => {
+    // console.log(x);
+  });
+
+classes.deleteOne({ name: "Class 10" }).then((x) => {
+  console.log(x);
+});
+
+// classes.deleteMany().then(x => {
+//     // console.log(x);
 // })
-
-classes.deleteById(["17f37d00d29117e5d0ce35", "17f37c694052c36340add59"]).then((x) => {
-    console.log(x);
-})
-
-classes.deleteOne({ name: "Class 10" }).then(x => {
-    console.log(x);
-})
-
-classes.deleteMany().then(x => {
-    console.log(x);
-})
 
 // classes.drop()
